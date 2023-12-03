c739de4749da788d159ed4be90882772674fdb0b
Aleksander Merhar <aleksander.merhar@spica.com>
2023-12-04 00:02:44 +0100

    feat: day 1, part 2 WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Day1Component } from './pages/day-1/day-1.component';
+import { Day12015Component } from './pages/day-1-2015/day-1-2015.component';
 
 export const routes: Routes = [
     {
@@ -8,6 +9,11 @@ export const routes: Routes = [
             path: '1',
             component: Day1Component,
             title: 'Day 1'
-        }]
+        }],
+    },
+    {
+        path: '2015-day-1',
+        component: Day12015Component,
+        title: '2015 Day 1'
     }
-    ];
+];
--- src/app/pages/day-1/day-1.component.ts
+++ src/app/pages/day-1/day-1.component.ts
@@ -12,7 +12,20 @@ export class Day1Component {
   protected input: string = ''
   protected output: number = 0;
 
-  private readonly reg = new RegExp('^[0-9]+$');
+  private readonly regCheckIfInt = new RegExp('^[0-9]+$');
+
+  
+  private readonly stringToNumberMap: any= {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+  };
 
 
   constructor(httpClient: HttpClient) {
@@ -26,20 +39,73 @@ export class Day1Component {
         const generatedNumbers: number[] = [];
 
         inputSplitByNewLine.forEach(line => {
+          line = line.trim();
           let firstNumber: number | null = null;
           let lastNumber: number | null = null;
           
-          for (let i = 0; i < line.length; i++) {
-            const numberCandidate = Number.parseInt(line[i]);
-            if(this.reg.test(numberCandidate.toString()) === false)
-              continue;
-
-            if(firstNumber === null)
-              firstNumber = numberCandidate;
-            
-
-            lastNumber = numberCandidate;
+          let map: any = {
+            'one': null,
+            'two': null,
+            'three': null,
+            'four': null,
+            'five': null,
+            'six': null,
+            'seven': null,
+            'eight': null,
+            'nine': null,
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+            5: null,
+            6: null,
+            7: null,
+            8: null,
+            9: null,
           }
+
+          Object.keys(map).forEach(key => {
+            const searchTerm = key;
+            const indexOfFirst = line.indexOf(searchTerm);
+            map[key] = indexOfFirst;
+          });
+
+          let min = Number.MAX_SAFE_INTEGER;
+          let minKey = '';
+          let max = -2;
+          let maxKey = '';
+          Object.keys(map).forEach(key => {
+            const value = map[key];
+            if(value === null || value === -1)
+              return;
+            
+            if(value < min){
+              min = value;
+              minKey = key;
+            }
+            if (value > max) {
+              max = value;
+              maxKey = key;
+            }
+          });
+
+
+          if (minKey === '') return;
+
+          if(!this.regCheckIfInt.test(minKey)){
+            firstNumber = this.stringToNumberMap[minKey as string];
+          } else {
+            firstNumber = Number.parseInt(minKey);
+          }
+
+          if(maxKey !== '')
+            if(!this.regCheckIfInt.test(maxKey)){
+              lastNumber = this.stringToNumberMap[maxKey as string];
+            } else {
+              lastNumber = Number.parseInt(maxKey);
+            }
+          else
+            lastNumber = firstNumber;
 
           let generatedNumber: number = 0;
 
@@ -49,8 +115,8 @@ export class Day1Component {
           } else if (firstNumber !== null) {
             const concatFirstNumberTwice = firstNumber.toString() + firstNumber.toString();
             generatedNumber = Number.parseInt( concatFirstNumberTwice);
-
           }
+
 
           generatedNumbers.push(generatedNumber);
 
