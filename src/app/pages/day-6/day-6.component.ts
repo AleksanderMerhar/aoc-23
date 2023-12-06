8aa7a94563fb16a2342989c092e120f8c4cfbb5a
Aleksander Merhar <aleksander.merhar@spica.com>
2023-12-06 19:27:21 +0100

    feat: day 6, part 2

--- src/app/pages/day-6/day-6.component.ts
+++ src/app/pages/day-6/day-6.component.ts
@@ -14,6 +14,7 @@ export class Day6Component {
   `Time:        48     93     85     95
 Distance:   296   1928   1236   1391`;
   protected output: number = 0;
+  protected output2: number = 0;
   
   constructor(){
 
@@ -21,6 +22,7 @@ Distance:   296   1928   1236   1391`;
     const inputSplit = this.input.split(/\r?\n|\r|\n/g);
     const times = getArrayOfNumbersFromLine(inputSplit[0].slice(5));
     const distances = getArrayOfNumbersFromLine(inputSplit[1].slice(9));
+
 
     const timesDistancesMap = new Map<number, number>();
     for(let i = 0; i < times.length && i < distances.length; i++) {
@@ -60,6 +62,23 @@ Distance:   296   1928   1236   1391`;
         this.output = this.output * winningCombinations;
       });
 
+      
+    //#region Part 2
+    const timePart2 = Number.parseInt(inputSplit[0].slice(5).replace(/ +/g, ""));
+    const distancePart2 = Number.parseInt(inputSplit[1].slice(9).replace(/ +/g, ""));
+
+    for(let i = 0; i < timePart2; i++) {
+      const speed = i;
+      const leftOverTime = timePart2 - i;
+
+      let myDistance = leftOverTime * speed;
+
+      if(myDistance > distancePart2) {
+        this.output2++;
+      }
+    }
+
+    //#endregion
 
   }
 }
